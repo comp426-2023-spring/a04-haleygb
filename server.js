2d7efcf5518b69c2b668c8dd13d95905fc16a740
Haley Bohn <haleygb@email.unc.edu>
2023-05-03 17:04:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,32 +8,20 @@ const minimist_module = minimist(process.argv.slice(2))
 
 const port = minimist_module.port || 5000
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-
-app.get('/app', (req, res) => {
-    res.status(200).send("200 OK");});
-
-app.get('app/rps/', (req, res) => {
-    res.status(200).send(rps());
-});
-
-app.get('app/rpsls/', (req, res) => {
-    res.status(200).send(rpsls());
-});
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
 
+app.get('/app', (req, res) => {
+    res.status(200).send("200 OK").end();});
 
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(rps()).end();});
 
-
-
-
-
-
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(rpsls()).end();});
